a57b2244ffe99a576a068012d2154d7010e5cbf6
Romanoz87 <chkaduaroman@gmail.com>
2024-12-11 15:53:30 +0400

    Update romahomework2_3.py

--- Roma_Homework02/romahomework2_3.py
+++ Roma_Homework02/romahomework2_3.py
@@ -1,7 +1,7 @@
 #დავალება3
 
 '''შექმენით ორი სტრინგის ტიპის ცვლადი st1 და st2, შეადარეთ ისინი is-ის გამოყენებით, 
-   თუ ემთხვევა გამოიტანეთ ტექსტი "Same object", წინააღმდეგ შემთხვევაში "Different object"
+თუ ემთხვევა გამოიტანეთ ტექსტი "Same object", წინააღმდეგ შემთხვევაში "Different object"
 '''
 st1 = "Learning Python in Step Academy"
 st2 = "There are 16 student in Python course"
